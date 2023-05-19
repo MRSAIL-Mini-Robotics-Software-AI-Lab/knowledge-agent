56ee0644453a3918789870ca31c2dcd6cebc1819
Ahmed Emad <50369848+ahmedemad242@users.noreply.github.com>
2023-05-19 18:15:51 +0300

    add the engine

---
+++ SPARQLEngine.py
@@ -0,0 +1,49 @@
+"""
+Sparql Engine class to run the queries
+"""
+import re
+from typing import List, Tuple
+import owlready2 as owl
+
+
+class SPARQLEngine:
+    """
+    Sparql Engine class to run the queries
+
+    Parameters
+    ----------
+    file_path : str
+        The path to the ontology file
+    """
+
+    def __init__(self, file_path: str):
+        self.world = owl.World()
+        self.onto = self.world.get_ontology(file_path).load()
+        self.graph = self.world.as_rdflib_graph()
+
+    def run_query(self, query: str) -> Tuple[List[str], List[List[str]]]:
+        """
+        Run a sparql query
+
+        Parameters
+        ----------
+        query : str
+            The sparql query
+
+        Returns
+        -------
+        Tuple[List[str], List[List[str]]]
+            The headers and results of the query
+        """
+        results = self.graph.query(query)
+        headers = [str(header) for header in results.vars]
+
+        parsed_results = []
+        for row in results:
+            to_add = []
+            for element in row:
+                parsed = element.n3().replace(">", "").replace("<", "")
+                parsed = re.sub("http:.*#", "", parsed)
+                to_add.append(parsed)
+            parsed_results.append(to_add)
+        return headers, parsed_results
